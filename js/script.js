636e4a3d7a7614d6cca57bc1f6af54d66bdbd533
SANDIP SADHUKHAN <sandippakhanna@gmail.com>
2020-07-30 16:14:35 +0530

    sem1,2,3 added

--- js/script.js
+++ js/script.js
@@ -1,5 +1,6 @@
 const rollNumberUI = document.getElementById('roll_number');
 const submitBtnUI = document.getElementById('submitbtn');
+const semUI = document.getElementById('sem')
 const homePage = document.getElementById('homepage');
 const resultPage = document.getElementById('resultpage');
 const resetBtnUI = document.getElementById('resetBtn');
@@ -7,6 +8,7 @@ const downloadBtnUI = document.getElementById('downloadBtn');
 const resultName = document.getElementById('result-name');
 const resultRollNo = document.getElementById('result-rollno');
 const resultCgpa = document.getElementById('result-cgpa');
+const resultSem = document.getElementById('result-sem');
 
 // Download global details
 downloadFileName = ''
@@ -28,25 +30,29 @@ const downloadPdf = () => {
 
 const submitBtnHandler = async()  => {
     let rollNumber = rollNumberUI.value;
+    let sem = semUI.value;
     // validation
     if(rollNumber === ''){
         return;
     }
-    fetch(`./../studentInfo/${rollNumber}_info.json`)
+    fetch(`./../studentInfo/studentInfo.json`)
         .then(res => res.json())
         .then(data => {
             // update the result page
-            resultName.innerText = data['name'];
-            resultRollNo.innerText = data['rollNo'];
-            resultCgpa.innerText = data['cgpa'];
+            semData = data[sem]
+            result = semData[rollNumber]
+            resultName.innerText = result['name'];
+            resultRollNo.innerText = result['rollNo'];
+            resultCgpa.innerText = result['cgpa'];
+            resultSem.innerText = sem.slice(-2)
             // Update download links
-            downloadFileName = data['name'];
-            downloadFileURL = `./../pdfs/${data['rollNo']}_marksheet.pdf`; 
+            downloadFileName = result['name'] + `_${sem}`;
+            downloadFileURL = `./../results/${sem}/${result['rollNo']}_marksheet.pdf`; 
             // home page vanish and result page shown
             homePage.style.display="none";
             resultPage.style.display="flex";
         })
-        .catch(error => {return;});
+        .catch(error => {console.log("Data not found!")});
 }
 
 const resetBtnHandler = () => {
--- python work/extractDataFromPdf.py
+++ python work/extractDataFromPdf.py
@@ -7,6 +7,9 @@ from io import StringIO
 import os
 import json
 
+def sortFunc(file):
+    return int(file.split("_marksheet.pdf")[0])
+
 def convert_pdf_to_txt(path):
     rsrcmgr = PDFResourceManager()
     retstr = StringIO()
@@ -31,35 +34,99 @@ def convert_pdf_to_txt(path):
     return text
 
 
-def findStudentInfo(fileName):
-    data = convert_pdf_to_txt(fileName)
-    # find sgpa
-    sep = data.split('ODD. (3rd) SEMESTER :')
-    sep2 = sep[1].split('\n')
-    cgpa = float(sep2[0])
-    # find name
-    sep = data.split('NAME : ')
-    sep2 = sep[1].split('\n')
-    name = sep2[0]
-    # find rollNo
-    sep = data.split('ROLL NO. : ')
-    sep2 = sep[1].split('\n')
-    rollNum = sep2[0]
+def findStudentInfo(fileName, sem):
+    # Main Output
+    cur_path = os.path.dirname(__file__)
+    file_path = os.path.relpath(f'./{sem}/{fileName}', cur_path)
+    data = convert_pdf_to_txt(file_path)
+    if sem == 'SM03':
+        # find sgpa
+        sep = data.split('ODD. (3rd) SEMESTER :')
+        sep2 = sep[1].split('\n')
+        try:
+            cgpa = float(sep2[0])
+        except:
+            cgpa = "Incomplete"
+        # find name
+        sep = data.split('NAME : ')
+        sep2 = sep[1].split('\n')
+        name = sep2[0]
+        # find rollNo
+        sep = data.split('ROLL NO. : ')
+        sep2 = sep[1].split('\n')
+        rollNum = sep2[0]
+    elif sem == 'SM02':
+        # find sgpa
+        sep = data.split('EVEN (2nd) SEMESTER :')
+        sep2 = sep[1].split('\n')
+        try:
+            cgpa = float(sep2[0])
+        except:
+            cgpa = "Incomplete"
+        # find name
+        sep = data.split('NAME : ')
+        sep2 = sep[1].split('\n')
+        name = sep2[0]
+        # find rollNo
+        sep = data.split('ROLL NO. : ')
+        sep2 = sep[1].split('\n')
+        rollNum = sep2[0] 
+        
+    elif sem == 'SM01':
+        # find sgpa
+        sep = data.split('ODD. (1st) SEMESTER :')
+        sep2 = sep[1].split('\n')
+        try:
+            cgpa = float(sep2[0])
+        except:
+            cgpa = "Incomplete"
+
+        # find name
+        sep = data.split('NAME : ')
+        sep2 = sep[1].split('\n')
+        name = sep2[0]
+        # find rollNo
+        sep = data.split('ROLL NO. : ')
+        sep2 = sep[1].split('\n')
+        rollNum = sep2[0]
 
     studentData = {
         'name':name,
         'rollNo':rollNum,
         'cgpa':cgpa,
     }
-    with open(f'{studentData["rollNo"]}_info.json','w', encoding='utf-8') as fin:
-        fin.write(json.dumps(studentData))
+    # with open(f'{studentData["rollNo"]}_info.json','w', encoding='utf-8') as fin:
+    #     fin.write(json.dumps(studentData))
+    return studentData
+
+def saveIntoFile(semFolders):
+    mainOutput = {}
+    for sem in semFolders:
+        allFiles = []
+
+        for file in os.listdir(f'./{sem}/'):
+            allFiles.append(file)
+
+        allFiles.sort(key=sortFunc)
+        mainDict = {}
+        for file in allFiles:
+            rollNo = int(file.split("_marksheet.pdf")[0])
+            content = findStudentInfo(file, sem)
+            mainDict[rollNo] = content
+            print(f"{rollNo} Done!")
+        mainOutput[sem] = mainDict
+    with open('studentInfo.json', 'w', encoding='utf-8') as f:
+        f.write(json.dumps(mainOutput))
+
 
 #main fun
 # delete previous info file
-for file in os.listdir():
-    if "_info.json" in file:
-        os.remove(file)
-# call all for pdf
-for file in os.listdir():
-    if "_marksheet.pdf" in file:
-        findStudentInfo(file)
+# for file in os.listdir():
+#     if "_info.json" in file:
+#         os.remove(file)
+if os.path.exists('studentInfo.json'):
+    os.remove('studentInfo.json')
+    
+semFolders = ['SM01', 'SM02', 'SM03']
+saveIntoFile(semFolders)
+# findStudentInfo(file)
